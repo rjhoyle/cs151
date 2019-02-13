2e84245f4d208b75360390461a510be435bce037
Roberto Hoyle <rhoyle@oberlin.edu>
2019-02-13 09:49:06 -0500

    Fixed in indentation

--- Student.java
+++ Student.java
@@ -6,32 +6,32 @@
 
 
 public class Student extends Person {
-    private int year;
-    
-    /** Constructor
-     * 
-     * @param fname String First Name
-     * @param lname String Last Name
-     * @param year int Year of Graduation
-     */
-    public Student(String fname, String lname, int year) {
-	super(fname, lname);
-	this.year = year;
-    }
-    
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	private int year;
 
-	Student s1 = new Student("Bob", "Geitz", 1975);
-	Person p1 = new Person("Roberto", "Hoyle");
-	
-	if (s1.equals(p1)) {
-	    System.out.println("s1 equals p1");
+	/** Constructor
+	 * 
+	 * @param fname String First Name
+	 * @param lname String Last Name
+	 * @param year int Year of Graduation
+	 */
+	public Student(String fname, String lname, int year) {
+		super(fname, lname);
+		this.year = year;
 	}
-	
-	if (s1 instanceof Person) {
-	    System.out.println("Students are people too!");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student s1 = new Student("Bob", "Geitz", 1975);
+		Person p1 = new Person("Roberto", "Hoyle");
+
+		if (s1.equals(p1)) {
+			System.out.println("s1 equals p1");
+		}
+
+		if (s1 instanceof Person) {
+			System.out.println("Students are people too!");
+		}
 	}
-    }
 
 }
